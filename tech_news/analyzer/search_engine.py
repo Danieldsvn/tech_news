182d4e0a8d4fd87c6652e71a677c0bba57d36870
Daniel Saraiva <danieldsvn@gmail.com>
2023-03-15 12:06:41 -0300

    Implementado funções auxiliares de validação de data que serão usadas na função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -8,13 +8,30 @@ def search_by_title(title):
         if title.lower() in content["title"].lower():
             result.append((content["title"], content["url"]))
 
-    print(find_news())
     return result
+
+
+def is_date_iso_format(date: str):
+    date_splited = date.split("-")
+    if len(date_splited) != 3:
+        raise ValueError
+    year, month, day = date_splited
+    if len(year) != 4 or len(month) != 2 or len(month) != 2:
+        raise ValueError
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    is_date_valid(year, month, day)
+
+
+def is_date_valid(year, month, day):
+    if year < 0 or month > 12 or day > 31:
+        raise ValueError
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    print(find_news())
 
 
 # Requisito 9
