cc83599ac27889ff226a4cabd00bd09fce612c0c
Daniel Saraiva <danieldsvn@gmail.com>
2023-03-16 12:29:45 -0300

    Implementada função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -14,24 +14,39 @@ def search_by_title(title):
 def is_date_iso_format(date: str):
     date_splited = date.split("-")
     if len(date_splited) != 3:
-        raise ValueError
+        raise ValueError("Data inválida")
     year, month, day = date_splited
     if len(year) != 4 or len(month) != 2 or len(month) != 2:
-        raise ValueError
+        raise ValueError("Data inválida")
     year = int(year)
     month = int(month)
     day = int(day)
     is_date_valid(year, month, day)
+    year, month, day = date.split("-")
+    date_formated = f"{day}/{month}/{year}"
+    return date_formated
 
 
 def is_date_valid(year, month, day):
     if year < 0 or month > 12 or day > 31:
-        raise ValueError
+        raise ValueError("Data inválida")
+    if month == 2 and day > 29:
+        raise ValueError("Data inválida")
+    return None
 
 
 # Requisito 8
 def search_by_date(date):
-    print(find_news())
+    try:
+        date_formated = is_date_iso_format(date)
+        result = []
+        for content in find_news():
+            if date_formated == content["timestamp"]:
+                result.append((content["title"], content["url"]))
+        return result
+    except ValueError as error:
+        print(error)
+        raise
 
 
 # Requisito 9
